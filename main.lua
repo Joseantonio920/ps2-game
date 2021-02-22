8e8f334d8a083e4fdb14572bc4d10893d74e4f98
Jose925 <josediscord9@gmail.com>
2021-02-22 12:36:18 -0300

    jogo

--- main.lua
+++ main.lua
@@ -1,14 +1,13 @@
 branco=Color.new(255, 255, 255)
 preto=Color.new(0, 0, 0)
-function draw()
-	screen:print(0, 0, "ola mundo", branco)
-end
-function update()
-	
-end
+
 while true do
+  --draw
 	screen:clear(preto)
-	draw()
+	
+	screen:print(0, 0, "ola mundo", branco)
+	
 	screen.flip()
-	update()
+	--update
+	
 end
